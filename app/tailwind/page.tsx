0cc0bb63f0e19066ea611c5ba5fde0ee88cadda3
math2do <mathura.dakshana15@gmail.com>
2024-02-06 18:56:37 +0530

    add custom styles

---
+++ app/tailwind/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tailwind = () => {
+  return (
+    <div>
+      <p className="">
+        Whereas disregard and contempt for human rights have resulted
+      </p>
+    </div>
+  );
+};
+
+export default Tailwind;
